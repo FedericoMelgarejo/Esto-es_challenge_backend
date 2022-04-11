80c145a967083ffe52bbeffd3ed5b0d08836248d
FedericoMelgarejo <f.n.melgarejo@gmail.com>
2022-04-11 01:52:38 -0300

    updated projects model and migration

--- src/database/models/projects.js
+++ src/database/models/projects.js
@@ -1,30 +1,31 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Projects extends Model {
-    /*
+  class projects extends Model {
+    /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Projects.belongsToMany(models.User, {
-        as: "contributors",
-        through: "UserProject",
+      projects.belongsTo(models.users, {
+        as: "Project Manager",
+        foreignKey: "projectManagerId",
       });
-      Projects.belongsTo(models.User, {
-        as: "PM",
-        foreignKey: "fk_project_manager_id",
+      projects.belongsTo(models.users, {
+        as: "Contributor",
+        foreignKey:"contributorId",
       });
     }
   }
-  Projects.init(
+  projects.init(
     {
       name: DataTypes.STRING,
       description: DataTypes.STRING,
-      status: DataTypes.INTEGER, //1 = active, 0 = inactive
-      fk_project_manager_id: DataTypes.STRING,
-      cretedAt: DataTypes.DATE,
+      projectManagerId: DataTypes.INTEGER,
+      contributorId: DataTypes.INTEGER,
+      status: DataTypes.BOOLEAN,
+      createdAt: DataTypes.DATE,
       updatedAt: DataTypes.DATE,
       deletedAt: DataTypes.DATE,
     },
@@ -32,8 +33,8 @@ module.exports = (sequelize, DataTypes) => {
       sequelize,
       timestamps: true,
       paranoid: true,
-      modelName: "Projects",
+      modelName: "projects",
     }
   );
-  return Projects;
+  return projects;
 };
